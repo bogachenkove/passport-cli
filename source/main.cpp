8fd1e98841fbb132bb2924687d6a6a5e811f2477
bogachenko <bogachenkove@gmail.com>
2026-02-16 16:18:10 +0300

    add package metadata
    - Add launch attributes
    - Add package metadata
    - Update build parameters to include package metadata

--- source/main.cpp
+++ source/main.cpp
@@ -2,9 +2,13 @@
 #include "modules/ui/console_terminal.hpp"
 #include "modules/crypto/crypto_service.hpp"
 #include "modules/storage/file_database.hpp"
+#include "modules/core/metadata.hpp"
 #include <memory>
 
-int main() {
+int main(int argc, char* argv[]) {
+	if (handle_metadata_flags(argc, argv)) {
+		return 0;
+	}
 	auto terminal = std::make_shared<ui::ConsoleTerminal>();
 	auto crypto = std::make_shared<security::crypto::SodiumCryptoService>();
 	auto database = std::make_shared<security::storage::FileDatabase>(crypto);
---
+++ source/modules/core/metadata.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#define ProductName "Passport CLI"
+#define FileDescription "Secure Password Manager"
+#define ProductVersion "0.0.0.7"
+#define License "MIT License"
+#define Copyright "Copyright © 2026 Bogachenko Vyacheslav. All rights reserved."
+#define Author "Bogachenko Vyacheslav"
+#define Contact "bogachenkove@outlook.com"
+#define Homepage "https://github.com/bogachenkove/passport-cli"
+
+inline bool handle_metadata_flags(int argc, char* argv[]) {
+	for (int i = 1; i < argc; ++i) {
+		if (std::strcmp(argv[i], "--version") == 0) {
+			std::cout << ProductName << " version " << ProductVersion << std::endl;
+			return true;
+		}
+		if (std::strcmp(argv[i], "--homepage") == 0) {
+			std::cout << Homepage << std::endl;
+			return true;
+		}
+		if (std::strcmp(argv[i], "--license") == 0) {
+			std::cout << License << std::endl;
+			return true;
+		}
+		if (std::strcmp(argv[i], "--help") == 0) {
+			std::cout << "Available metadata flags:\n"
+					  << "  --version   Show version information\n"
+					  << "  --homepage  Show project homepage URL\n"
+					  << "  --license   Show license information\n"
+					  << "  --help      Show this help message\n";
+			return true;
+		}
+	}
+	return false;
+}
