0abe977077205b7178b7ee2eec1a2d3184427984
bogachenko <bogachenkove@gmail.com>
2026-02-25 23:56:46 +0300

    enhance license display and flag handling
    - Add `read_license_file()` to display full license text from `LICENSE.txt`
    - Fall back to online URL if file not found
    - Improve `--license` output with product name, copyright, and license text
    - Handle unknown command-line flags with error message
    - Update homepage URL from `/passport-cli` to `/passport`
    - Tweak version output format

--- source/modules/core/metadata.hpp
+++ source/modules/core/metadata.hpp
@@ -1,19 +1,32 @@
 #pragma once
 #include <iostream>
+#include <string>
 #include <cstring>
+#include <fstream>
+#include <sstream>
 #define ProductName "Passport CLI"
 #define FileDescription "Secure Password Manager"
 #define ProductVersion "0.0.0.10"
 #define License "MIT License"
-#define Copyright "Copyright © 2026 Bogachenko Vyacheslav. All rights reserved."
+#define Copyright "Copyright (c) 2026 Bogachenko Vyacheslav. All rights reserved."
 #define Author "Bogachenko Vyacheslav"
 #define Contact "bogachenkove@outlook.com"
-#define Homepage "https://github.com/bogachenkove/passport-cli"
+#define Homepage "https://github.com/bogachenkove/passport"
+#define LICENSE_FILE_PATH "LICENSE.txt"
 
+inline std::string read_license_file() {
+	std::ifstream file(LICENSE_FILE_PATH);
+	if (!file.is_open()) {
+		return "Please read the license agreement at https://raw.githubusercontent.com/bogachenkove/passport/stable/LICENSE.md";
+	}
+	std::stringstream buffer;
+	buffer << file.rdbuf();
+	return buffer.str();
+}
 inline bool handle_metadata_flags(int argc, char* argv[]) {
 	for (int i = 1; i < argc; ++i) {
 		if (std::strcmp(argv[i], "--version") == 0) {
-			std::cout << ProductName << " version " << ProductVersion << std::endl;
+			std::cout << ProductName << ", version: " << ProductVersion << std::endl;
 			return true;
 		}
 		if (std::strcmp(argv[i], "--homepage") == 0) {
@@ -21,15 +34,23 @@ inline bool handle_metadata_flags(int argc, char* argv[]) {
 			return true;
 		}
 		if (std::strcmp(argv[i], "--license") == 0) {
-			std::cout << License << std::endl;
+			std::cout << ProductName << "\n" << Copyright <<
+				"\nThis program is free software released under the " << License
+				"\nUse, modification and distribution are permitted under the terms of the license below.\n\n";
+			std::cout << read_license_file() << std::endl;
 			return true;
 		}
 		if (std::strcmp(argv[i], "--help") == 0) {
 			std::cout << "Available metadata flags:\n"
-					  << "  --version   Show version information\n"
-					  << "  --homepage  Show project homepage URL\n"
-					  << "  --license   Show license information\n"
-					  << "  --help      Show this help message\n";
+				<< "  --version   Show version information\n"
+				<< "  --homepage  Show project homepage URL\n"
+				<< "  --license   Show license information\n"
+				<< "  --help      Show this help message\n";
+			return true;
+		}
+		if (argv[i][0] == '-') {
+			std::cerr << "Unknown option: " << argv[i] << "\n";
+			std::cerr << "Try '--help' for more information.\n";
 			return true;
 		}
 	}
