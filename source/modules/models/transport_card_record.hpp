a7c847ff949d1dd8897083ebf95771c1940319b3
bogachenko <bogachenkove@gmail.com>
2026-02-27 02:27:01 +0300

    save bytes

--- source/modules/models/transport_card_record.hpp
+++ source/modules/models/transport_card_record.hpp
@@ -7,12 +7,12 @@ namespace domain::models {
 
 	struct TransportCardRecord {
 		std::uint64_t date = 0;
-		std::string   card_number;
-		std::string   barcode;
-		std::string   expiry;
-		std::string   holder;
-		std::string   cvv;
-		std::string   note;
+		std::string card_number;
+		std::string barcode;
+		std::string expiry;
+		std::string holder;
+		std::string cvv;
+		std::string note;
 	};
 
 }
